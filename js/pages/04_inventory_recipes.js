ba5140d0866dc67c521b0168182620c108e56e2b
Aaom2316 <96713764+Aaom2316@users.noreply.github.com>
2026-09-10 18:01:05 +0700

    Add files via upload

--- js/pages/04_inventory_recipes.js
+++ js/pages/04_inventory_recipes.js
@@ -27,7 +27,7 @@ POS.pages.inventoryRecipes = async function(){
             font-weight:800;
             color:#1f2937;
           ">
-            📦 Movement
+            🍳 Recipe
           </h1>
 
           <p class="page-subtitle" style="
